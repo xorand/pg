48c28fe802c490bfadc6e24ce3217301ae31a200
xorand <xorand@gmail.com>
2017-02-09 12:21:43 +0300

    modified:   bk.sh - fix reading config file if running with absolute path new file:   readme.md - readme

--- bk.sh
+++ bk.sh
@@ -5,7 +5,7 @@ msg () {
 }
 
 # reading config file
-SP=$(dirname "$SCRIPT")
+SP=$(dirname "$0")
 if [ "$SP" = "." ]; then
     SP=`pwd`
 fi
@@ -21,7 +21,7 @@ for DB in ${DBS}; do
     msg "dumping database [$DB] ..."
     /usr/bin/time -f%E /usr/bin/pg_dump --user postgres -Fc $DB > $FNAME
     msg "copying database [$DB] ..."
-    /usr/bin/time -f%E /usr/bin/scp -q $FNAME $HOST:$PATH
+    /usr/bin/time -f%E /usr/bin/scp -q $FNAME $HOST:$RPATH
     /bin/rm -f $FNAME 
     msg "backup [$DB] done"
     printf "\n"
@@ -29,7 +29,7 @@ done
 
 if [ -z "$1" ]; then
     msg "cleaning old backups ..."
-    /usr/bin/time -f%E /usr/bin/ssh -q $HOST "find $PATH -type f -mtime +$DAYS -print0 | xargs -0 rm -f"
+    /usr/bin/time -f%E /usr/bin/ssh -q $HOST "find $RPATH -type f -mtime +$DAYS -print0 | xargs -0 rm -f"
     msg "all done"
     printf "\n"
 fi
---
+++ readme.md
@@ -0,0 +1,14 @@
+# scripts for automate postgresql backup/restore operations
+
+# bk.sh
+backup script, takes settings from pg.conf
+command-line paramaeter $1 override databases to backup from config file
+
+# 2ch.sh
+copy $1 database to $2 database
+$2 database recreated during process - be careful
+
+# rs.sh
+restore to $1 database $2 dump from remote location
+takes settings from pg.conf
+
